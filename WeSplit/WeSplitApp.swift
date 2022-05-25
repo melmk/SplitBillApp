6bebc21efd43843ee3680fd0d00b47b9fdf38dec
MelMK <melmkwong@gmail.com>
2022-05-25 21:27:16 +1000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Melissa Kwong on 25/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
